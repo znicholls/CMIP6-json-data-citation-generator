e41dd87c9df4cdc9a3ea159280af6009bf7c5df9
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-06-14 01:17:37 +0200

    Add failing test for split CMIP6 file name

--- CMIP6_json_data_citation_generator/__init__.py
+++ CMIP6_json_data_citation_generator/__init__.py
@@ -1,5 +1,32 @@
+from os import listdir
+
+class CMIPPathHandler():
+    def __init__(self):
+        self._path_to_handle = ''
+
+    def get_split_CMIP6_filename(self, file_name=None):
+        return {}
 
 class jsonGenerator():
     def __init__(self):
         self.unique_scenario_ids = []
         self.source_id_written_for = []
+
+    def get_unique_source_ids_in_dir(self, dir_to_search=None):
+        self.unique_scenario_ids = []
+        self.add_unique_source_ids_in_dir_to_unique_scenario_ids(dir_to_add=dir_to_search)
+        return self.unique_scenario_ids
+
+    def add_unique_source_ids_in_dir_to_unique_scenario_ids(self, dir_to_add=None):
+        for file_name in listdir(dir_to_add):
+            if file_name.endswith('.nc'):
+                try:
+                    source_id = self.split_CMIP6_filename(file_name)['source_id']
+                    if source_id not in self.unique_scenario_ids:
+                        self.unique_scenario_ids.append(source_id)
+                except Exception:
+                    pass
+
+    def split_CMIP6_filename(self, file_name=None):
+        PathHandler = CMIPPathHandler()
+        return PathHandler.get_split_CMIP6_filename(file_name=file_name)
