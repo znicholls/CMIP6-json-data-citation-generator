82dc12064219db1986056f4769e469b5b12ee167
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-06-27 14:33:37 +0200

    Accidental commit

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, isdir
 import sys
 
 import pytest
@@ -6,6 +6,7 @@ from mock import patch
 
 from CMIP6_json_data_citation_generator.upload_CMIP6_json_files import main
 from CMIP6_json_data_citation_generator.upload_CMIP6_json_files import upload
+from CMIP6_json_data_citation_generator.upload_CMIP6_json_files import get_files_to_upload
 
 command_line_command = "upload_CMIP6_json_files"
 
@@ -33,6 +34,20 @@ def test_upload_passing(mock_get_files_to_upload):
     upload('test/input', upload_all=True)
     mock_get_files_to_upload.assert_called_with("test/input", find_all=True)
 
-@patch('CMIP6_json_data_citation_generator.upload_CMIP6_json_files.get_files_to_upload')
-def test_all_switch(mock_get_files_to_upload):
-    assert True
+@patch('CMIP6_json_data_citation_generator.os.listdir')
+def test_get_files_to_upload(mock_listdir):
+    test_dir = 'input/dir'
+    test_files = ['a', 'b', 'c']
+    def mock_listdir_return(in_dir):
+        if in_dir == test_dir:
+            return test_files
+        else:
+            return []
+
+    mock_listdir.side_effect = mock_listdir_return
+    assert test_files == get_files_to_upload(test_dir, find_all=True)
+    assert 1 == len(get_files_to_upload(test_dir, find_all=False))
+
+    junk_dir = './junk/dir'
+    assert not isdir(junk_dir)
+    assert [] == get_files_to_upload(junk_dir, find_all=True)
