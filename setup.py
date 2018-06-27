53737471f06f87414512e526846b270aeb6a6862
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-06-27 22:46:46 +0200

    Remove pytest dependency as covered by pytest-cov

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     long_description=read('README.md'),
     url='https://gitlab.com/znicholls/CMIP6_data_file_renaming',
     install_requires=[
-        'pytest',
         'httplib2',
         'mock',
         'pyyaml',
