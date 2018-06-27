bdd9faf9c3ac93b58e8bceeeb1e18b9d66397fa3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-06-27 14:18:15 +0200

    Pass command line argument passing

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,25 +1,34 @@
 from os.path import join
+import sys
 
 import pytest
 from mock import patch
 
+from CMIP6_json_data_citation_generator.upload_CMIP6_json_files import main
 
-# from CMIP6_json_data_citation_generator import upload_CMIP6_json_files
-
+command_line_command = "upload_CMIP6_json_files"
 # mock sys.argv
 # mock list
 # mock uploader
 # ensure called and print did as expected
 
 @pytest.fixture
-def mock_sys():
-    return patch('upload_CMIP6_json_files.sys')
+def mock_sys_argv():
+    def _mock_sys(return_value):
+        return patch.object(sys, 'argv', return_value)
 
+    return _mock_sys
 
-def test_upload(mock_sys):
-    mock_sys.argv = []
-    print(mock_sys.argv)
-    mock_sys.argv = ['--all']
-    print(mock_sys.argv)
+@patch('CMIP6_json_data_citation_generator.upload_CMIP6_json_files.upload')
+def test_upload(mock_upload, mock_sys_argv):
+    # mock_sys_argv.argv = []
+    # main()
+    # mock_upload.assert_called_with([])
+    # with patch.object(sys, 'argv', ['all']):
+    with mock_sys_argv([command_line_command, "test/input"]):
+        main()
+        mock_upload.assert_called_with(input_dir="test/input", all=False)
 
-    mock_upload.assert_not_called()
+    with mock_sys_argv([command_line_command, '--all', 'test/input']):
+        main()
+        mock_upload.assert_called_with(input_dir="test/input", all=True)
