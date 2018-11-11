58e595af68c77e8e9d573ef2232fa552dc9fe813
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-11 12:06:30 +0000

    Pass validate yaml first test

---
+++ src/cmip6_data_citation_generator/validators.py
@@ -0,0 +1,82 @@
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+
+def validate_dependent_fields(data, dependent_fields, name):
+    error_msg = (
+        "The fields {} in {} are co-dependent, if you supply one of them, you must "
+        "supply all of them".format(dependent_fields, name)
+    )
+    for i, field in enumerate(dependent_fields):
+        if field in data:
+            for dependent_field in dependent_fields[i:]:
+                if dependent_field not in data:
+                    raise ValidationError(error_msg)
+
+
+class NameIdentifierSchema(Schema):
+    nameIdentifierScheme = fields.String(required=True)
+    pid = fields.String(required=True)
+    schemeURI = fields.Url(required=True)
+
+
+class ContributorSchema(Schema):
+    affiliation = fields.String()
+    contributorName = fields.String(required=True)
+    contributorType = fields.String(required=True)
+    email = fields.String()
+    familyName = fields.String()
+    givenName = fields.String()
+    nameIdentifier = fields.Nested(NameIdentifierSchema)
+
+    @validates_schema
+    def dependency_validation(self, data):
+        validate_dependent_fields(data, ["email", "familyName", "givenName"], "contributors")
+
+
+class CreatorSchema(Schema):
+    affiliation = fields.String()
+    creatorName = fields.String(required=True)
+    email = fields.String()
+    familyName = fields.String()
+    givenName = fields.String()
+    nameIdentifier = fields.Nested(NameIdentifierSchema)
+
+    @validates_schema
+    def dependency_validation(self, data):
+        validate_dependent_fields(data, ["email", "familyName", "givenName"], "creators")
+
+
+class FundingReferencesSchema(Schema):
+    funderName = fields.String(required=True)
+    funderIdentifier = fields.String()
+    funderIdentifierType = fields.String()
+
+    @validates_schema
+    def dependency_validation(self, data):
+        validate_dependent_fields(data, ["funderIdentifier", "funderIdentifierType"], "fundingReferences")
+
+
+class RelatedIdentifiersSchema(Schema):
+    relatedIdentifier = fields.String(required=True)
+    relatedIdentifierType = fields.String(required=True)
+    relationType = fields.String(required=True)
+
+
+class SubjectsSchema(Schema):
+    subject = fields.String(required=True)
+    schemeURI = fields.Url()
+    subjectScheme = fields.String()
+
+    @validates_schema
+    def dependency_validation(self, data):
+        validate_dependent_fields(data, ["schemeURI", "subjectScheme"], "subjects")
+
+
+class CitationSchema(Schema):
+    contributors = fields.Nested(ContributorSchema, required=True, many=True)
+    creators = fields.Nested(CreatorSchema, required=True, many=True)
+    fundingReferences = fields.Nested(FundingReferencesSchema, many=True)
+    relatedIdentifiers = fields.Nested(RelatedIdentifiersSchema, many=True)
+    subjects = fields.Nested(SubjectsSchema, required=True, many=True)
+    titles = fields.List(fields.String(), required=True)
+
--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -1,2 +1,4 @@
+from cmip6_data_citation_generator.validators import CitationSchema
+
 def test_load_template_yaml(valid_yaml):
     CitationSchema(strict=True).load(valid_yaml)
