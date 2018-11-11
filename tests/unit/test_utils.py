c2be1f3423ed151f51646849b3ce0a9cdd87ce37
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-11 15:39:29 +0000

    Add deep_substitute tests

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,22 @@
+from cmip6_data_citation_generator.utils import deep_substitute
+
+@pytest.mark.parametrize("input_value,expected", [
+    ("<source_id>", "rsouce_id"),
+    ("nor", "nor"),
+    (2, 2),
+    (["<source_id>", "<source_id>"], ["rsouce_id", "rsouce_id"]),
+    (["nor", "<activity_id>"], ["nor", "ractivity_id"]),
+    (["nor", "nor"], ["nor", "nor"]),
+    (["nor", "<source_id>", 3], ["nor", "rsouce_id", 3]),
+    ([{"nor": "<source_id>", "key": 3}], [{"nor": "rsouce_id", "key": 3}]),
+    ({"<activity_id>": "nor", "other_key": "<source_id>", "final_key": [{"bottom_key": "<institution_id>"}]}, {"<activity_id>": "nor", "other_key": "rsouce_id", "final_key": [{"bottom_key": "rinstitution_id"}]}),
+])
+def test_deep_substitue(input_value, expected):
+    subst_dict = {
+        "source_id": "rsouce_id",
+        "activity_id": "ractivity_id",
+        "institution_id": "rinstitution_id",
+    }
+
+    result = deep_substitute(input_value, subst_dict)
+    assert result == expected
