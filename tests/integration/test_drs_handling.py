54eca4f8bf71f6136b436fcbffd4dfcc2790da1e
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-12 20:08:46 +0000

    Add error messaging for directory scanning

--- tests/integration/test_drs_handling.py
+++ tests/integration/test_drs_handling.py
@@ -5,6 +5,7 @@ import pytest
 
 
 from conftest import (
+    TEST_DATA_ROOT_DIR,
     TEST_DATA_CMIP6_INPUT4MIPS_STYLE,
     TEST_DATA_CMIP6_OUTPUT_STYLE,
     TEST_DATA_FLATTISH_STYLE,
@@ -40,11 +41,7 @@ def test_get_unique_subjects_in_dir_cmip6input4mips_style_regexp_include():
         TEST_DATA_CMIP6_INPUT4MIPS_STYLE, "CMIP6input4MIPs", regexp=".*AerChemMIP.*"
     )
 
-    expected = sorted(
-        [
-            "input4MIPs.CMIP6.AerChemMIP.UoM.UoM-AIM-ssp370-lowNTCF-1-2-0",
-        ]
-    )
+    expected = sorted(["input4MIPs.CMIP6.AerChemMIP.UoM.UoM-AIM-ssp370-lowNTCF-1-2-0"])
 
     assert result == expected
 
@@ -72,7 +69,10 @@ def test_get_unique_subjects_in_dir_cmip6input4mips_style_regexp_exclude():
 
 def test_get_unique_subjects_in_dir_cmip6input4mips_style_keep_false():
     result = _get_unique_subjects_in_dir(
-        TEST_DATA_CMIP6_INPUT4MIPS_STYLE, "CMIP6input4MIPs", regexp=".*IMAGE.*", keep=False
+        TEST_DATA_CMIP6_INPUT4MIPS_STYLE,
+        "CMIP6input4MIPs",
+        regexp=".*IMAGE.*",
+        keep=False,
     )
 
     expected = sorted(
@@ -99,15 +99,36 @@ def test_get_unique_subjects_in_dir_cmip6output_style():
     assert result == expected
 
 
-# def test_get_unique_subjects_in_dir_flat_style():
-#     _get_unique_subjects_in_dir(TEST_DATA_CMIP6_INPUT4MIPS_STYLE, "CMIP6input4MIPs")
-#     assert False
+def test_get_unique_subjects_in_dir_flat_style():
+    error_msg = r"^.*" + "\n\n" + re.escape(
+        "Please note: "
+        "The CMIP6 data citation json generator only works with "
+        "filepaths which match a CMIP data reference syntax. Hence "
+        "we can't make a citation for you unless your files are "
+        "correctly named and sorted into appropriate directories. We "
+        "are considering upgrading so that citations can be made "
+        "independent of filename and directory structure. If this "
+        "functionality would be useful, please comment on this issue: "
+        "https://github.com/znicholls/CMIP6-json-data-citation-generator/issues/20"
+    )
+    with pytest.raises(ValueError, match=error_msg):
+        _get_unique_subjects_in_dir(TEST_DATA_FLATTISH_STYLE, "CMIP6input4MIPs")
 
 
-# def test_get_unique_subjects_in_dir_errors():
-#     assert False
-#     # nothing found
-#     # filename/path unreadable
+def test_get_unique_subjects_in_dir_cant_read_error():
+    error_msg = r"^.*" + "\n\n" + re.escape(
+        "Please note: "
+        "The CMIP6 data citation json generator only works with "
+        "filepaths which match a CMIP data reference syntax. Hence "
+        "we can't make a citation for you unless your files are "
+        "correctly named and sorted into appropriate directories. We "
+        "are considering upgrading so that citations can be made "
+        "independent of filename and directory structure. If this "
+        "functionality would be useful, please comment on this issue: "
+        "https://github.com/znicholls/CMIP6-json-data-citation-generator/issues/20"
+    )
+    with pytest.raises(ValueError, match=error_msg):
+        _get_unique_subjects_in_dir(TEST_DATA_ROOT_DIR, "CMIP6input4MIPs")
 
 
 def test_get_unique_subjects_in_dir_no_drs_error():
