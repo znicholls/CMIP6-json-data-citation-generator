01a4e9e9942760b9cf946d3ee75b5943229fc259
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-13 02:53:34 +0000

    Pass generate json test

--- src/cmip6_data_citation_generator/__init__.py
+++ src/cmip6_data_citation_generator/__init__.py
@@ -1,5 +1,9 @@
-from .io_dcg import load_and_validate_yaml
-from .drs_handling import _get_matching_paths_in_dir
+from os.path import join
+
+
+from .io_dcg import load_and_validate_yaml, write_json
+from .drs_handling import _get_matching_paths_in_dir, _get_subject_path, _get_ids_path
+from .utils import deep_substitute
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
@@ -7,10 +11,20 @@ del get_versions
 
 def generate_jsons(input_dir, template_yaml, drs, output_dir, regexp=".*", keep=True):
     template_yaml_dict = load_and_validate_yaml(template_yaml)
-    # import pdb
-    # pdb.set_trace()
-    # get all matching paths in dict
-    # loop over, get subject, if subject not there, substitute in yaml, write json
+    subjects_written = []
+
     for fp in _get_matching_paths_in_dir(input_dir, regexp=regexp, keep=keep):
-        pass
+        subject = _get_subject_path(fp, drs)
+        if subject in subjects_written:
+            continue
+
+        ids = _get_ids_path(fp, drs)
+        json_dict = deep_substitute(template_yaml_dict, ids)
+        if json_dict["subjects"][0]["subject"] != subject:
+            import pdb
+            pdb.set_trace()
+            raise NotImplementedError("Should reorder here")
+
+        write_json(json_dict, join(output_dir, "{}.json".format(subject)))
+
     return None
--- tests/integration/test_generate_jsons.py
+++ tests/integration/test_generate_jsons.py
@@ -1,6 +1,7 @@
 from os import remove
 from os.path import isfile
 import re
+import json
 
 
 import pytest
@@ -16,7 +17,14 @@ from cmip6_data_citation_generator import generate_jsons
 def test_get_unique_subjects_in_dir_cmip6output_style():
     generate_jsons(TEST_DATA_CMIP6_OUTPUT_STYLE, TEST_VALID_INPUT_YAML, "CMIP6output", ".", regexp=".*")
 
-    expected_file = "./CMIP6.DCPP.CNRM-CERFACS.CNRM-CM6-1.json"
+    expected_file = "./CMIP6.DCPP.CNRM-CERFACS.CNRM-CM6-1.dcppA-hindcast.json"
 
     assert isfile(expected_file)
+
+    written = json.loads(open(expected_file).read())
+
+    assert written["contributors"][0]["contributorName"] == "Meinshausen, Malte"
+    assert written["titles"] == ['CNRM-CM6-1 GHG concentrations']
+    assert written["subjects"][0]["subject"] == "CMIP6.DCPP.CNRM-CERFACS.CNRM-CM6-1.dcppA-hindcast"
+
     remove(expected_file)
