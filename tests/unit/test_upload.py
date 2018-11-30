034530e5d621c7c2570eb54d54b26bc1fe430e25
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-30 23:04:28 +1100

    Fill out tests for data citation upload

--- tests/unit/test_upload.py
+++ tests/unit/test_upload.py
@@ -1,5 +1,9 @@
 from unittest.mock import patch, MagicMock
 import json
+import subprocess
+
+
+import pytest
 
 
 from cmip6_data_citation_generator.upload import upload_jsons
@@ -8,43 +12,43 @@ from conftest import TEST_VALID_OUTPUT_JSON, TEST_DATA_ROOT_DIR
 
 @patch("cmip6_data_citation_generator.upload.netrc")
 @patch("cmip6_data_citation_generator.upload.Http")
-def test_upload_file(mock_Http, mock_netrc):
+@pytest.mark.parametrize(
+        "tinput,ttest,tcontent",
+        [(TEST_VALID_OUTPUT_JSON, False, "SUCCESS content"), (TEST_DATA_ROOT_DIR, True, "junk")],
+)
+def test_upload_file(mock_Http, mock_netrc, tinput, ttest, tcontent):
     tlogin = "login"
     tpassword = "pword"
 
-    mock_netrc = MagicMock()
-    mock_netrc.authenticators = MagicMock(return_value=(tlogin, "", tpassword))
+    class TResponse(object):
+        status = "status"
 
-    res = upload_jsons(TEST_VALID_OUTPUT_JSON)
-    assert res == 0
+    tresponse = TResponse()
+    tcontent = "SUCCESS content"
 
-    mock_netrc.assert_called_with("cera")
-    mock_add_credentials.assert_called_with(tlogin, tpassword)
-    mock_request.assert_called_with(
-        "http://ceracite.dkrz.de:5000/api/v1/citation",
+    with open(TEST_VALID_OUTPUT_JSON) as f:
+        expected_data = json.dumps(json.load(f))
+
+    if ttest:
+        expected_dest = "http://ceracite.dkrz.de:5000/api/v1/citation?test=1"
+    else:
+        expected_dest = "http://ceracite.dkrz.de:5000/api/v1/citation"
+
+    mock_netrc.return_value.authenticators.return_value = (tlogin, "", tpassword)
+    mock_Http.return_value.request.return_value = (tresponse, tcontent)
+
+    res = upload_jsons(tinput, test=ttest)
+    if tcontent.startswith("SUCCESS"):
+        assert res == 0
+    else:
+        assert res == 1
+
+    mock_netrc.return_value.authenticators.assert_called_with("cera")
+    mock_Http.return_value.add_credentials.assert_called_with(tlogin, tpassword)
+
+    mock_Http.return_value.request.assert_called_with(
+        expected_dest,
         "POST",
-        json.dumps(json.load(TEST_VALID_OUTPUT_JSON)),
-        headers={"Content-Type": "application/json"},
-    )
-
-
-@patch("cmip6_data_citation_generator.upload.netrc")
-@patch("cmip6_data_citation_generator.upload.Http")
-def test_upload_dir(mock_Http, mock_netrc):
-    tlogin = "login"
-    tpassword = "pword"
-
-    mock_netrc = MagicMock()
-    mock_netrc.authenticators = MagicMock(return_value=(tlogin, "", tpassword))
-
-    res = upload_jsons(TEST_DATA_ROOT_DIR, test=True)
-    assert res == 0
-
-    mock_netrc.assert_called_with("cera")
-    mock_add_credentials.assert_called_with(tlogin, tpassword)
-    mock_request.assert_called_with(
-        "http://ceracite.dkrz.de:5000/api/v1/citation?test=1",
-        "POST",
-        json.dumps(json.load(TEST_VALID_OUTPUT_JSON)),
+        expected_data,
         headers={"Content-Type": "application/json"},
     )
