6ee47509b888d7ba77716d70f91b06cccfe5ac31
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-12-01 00:56:43 +1100

    Fix behaviour of test to recognise test server does not have all files

--- src/cmip6_data_citation_generator/upload.py
+++ src/cmip6_data_citation_generator/upload.py
@@ -22,18 +22,21 @@ def upload_jsons(inputs, test=False):
 
     content_type_header = "application/json"
     url = "http://ceracite.dkrz.de:5000/api/v1/citation"
-    if test:
-        url = "{}?test=1".format(url)
 
     errors = False
+    if test:
+        print("Would upload the following files to:\n{}".format(url))
     for json_file in jsons:
+        if test:
+            print("    - {}".format(json_file))
+            continue
         with open(json_file) as f:
             data = json.load(f)
 
         response, content = http.request(
             url, "POST", json.dumps(data), headers={"Content-Type": content_type_header}
         )
-
+        content = content.decode("utf-8")
         print("{}, {}".format(response.status, content))
 
         errors = errors or not content.startswith("SUCCESS")
