dc405b4ef2096f7bf559221fa766ce2321c0e81a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-30 22:15:31 +1100

    Add first draft of uploader

--- src/cmip6_data_citation_generator/upload.py
+++ src/cmip6_data_citation_generator/upload.py
@@ -1,2 +1,41 @@
-def upload_jsons():
-    pass
+from os.path import isfile, isdir, join
+from glob import glob
+import json
+
+
+from netrc import netrc
+import httplib2
+from httplib2 import Http
+
+
+def upload_jsons(inputs, test=False):
+    if isfile(inputs):
+        jsons = [inputs]
+    else:
+        jsons = glob(join(inputs, "*.json"))
+
+    import pdb
+
+    pdb.set_trace()
+    httplib2.debuglevel = 0
+
+    info = netrc()
+    http = httplib2.Http()
+
+    login, account, password = info.authenticators("cera")
+    http.add_credentials(login, password)
+
+    content_type_header = "application/json"
+    url = "http://ceracite.dkrz.de:5000/api/v1/citation"
+    if test:
+        url = "{}?test=1".format(url)
+
+    for json_file in jsons:
+        with open(json_file) as f:
+            data = json.load(f)
+
+        response, content = http.request(
+            url, "POST", json.dumps(data), headers={"Content-Type": content_type_header}
+        )
+
+        print("{}\n{}".format(response.status, content))
--- tests/unit/test_upload.py
+++ tests/unit/test_upload.py
@@ -6,15 +6,14 @@ from cmip6_data_citation_generator.upload import upload_jsons
 from conftest import TEST_VALID_OUTPUT_JSON, TEST_DATA_ROOT_DIR
 
 
-@patch("cmip6_data_citation_generator.netrc.netrc")
-@patch.object("cmip6_data_citation_generator.httplib2.Http", "add_credentials")
-@patch.object("cmip6_data_citation_generator.httplib2.Http", "request")
-def test_upload_file(mock_request, mock_add_credentials, mock_netrc):
+@patch("cmip6_data_citation_generator.upload.netrc")
+@patch("cmip6_data_citation_generator.upload.Http")
+def test_upload_file(mock_Http, mock_netrc):
     tlogin = "login"
     tpassword = "pword"
 
     mock_netrc = MagicMock()
-    mock_netrc.authenticators = MagicMock(return_value = (tlogin, "", tpassword))
+    mock_netrc.authenticators = MagicMock(return_value=(tlogin, "", tpassword))
 
     res = upload_jsons(TEST_VALID_OUTPUT_JSON)
     assert res == 0
@@ -25,19 +24,18 @@ def test_upload_file(mock_request, mock_add_credentials, mock_netrc):
         "http://ceracite.dkrz.de:5000/api/v1/citation",
         "POST",
         json.dumps(json.load(TEST_VALID_OUTPUT_JSON)),
-        headers={"Content-Type": "application/json"}
+        headers={"Content-Type": "application/json"},
     )
 
 
-@patch("cmip6_data_citation_generator.netrc.netrc")
-@patch.object("cmip6_data_citation_generator.httplib2.Http", "add_credentials")
-@patch.object("cmip6_data_citation_generator.httplib2.Http", "request")
-def test_upload_dir(mock_request, mock_add_credentials, mock_netrc):
+@patch("cmip6_data_citation_generator.upload.netrc")
+@patch("cmip6_data_citation_generator.upload.Http")
+def test_upload_dir(mock_Http, mock_netrc):
     tlogin = "login"
     tpassword = "pword"
 
     mock_netrc = MagicMock()
-    mock_netrc.authenticators = MagicMock(return_value = (tlogin, "", tpassword))
+    mock_netrc.authenticators = MagicMock(return_value=(tlogin, "", tpassword))
 
     res = upload_jsons(TEST_DATA_ROOT_DIR, test=True)
     assert res == 0
@@ -48,6 +46,5 @@ def test_upload_dir(mock_request, mock_add_credentials, mock_netrc):
         "http://ceracite.dkrz.de:5000/api/v1/citation?test=1",
         "POST",
         json.dumps(json.load(TEST_VALID_OUTPUT_JSON)),
-        headers={"Content-Type": "application/json"}
+        headers={"Content-Type": "application/json"},
     )
-
